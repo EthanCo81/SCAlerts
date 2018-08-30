4faa7d7baa1d50723696b11fb356ee44acbf7bf7
vn505h2 <vn505h2@DW4CXDEVUS0112.corp.dom>
2018-08-30 15:08:43 -0500

    init

---
+++ CPUAlert/src/main/java/com/example/demo/CpuAlertApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CpuAlertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CpuAlertApplication.class, args);
+	}
+}
