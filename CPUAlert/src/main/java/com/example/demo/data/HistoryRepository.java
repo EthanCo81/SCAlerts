0c574076cf711918c38707dc432cde19b366cdc2
vn505h2 <vn505h2@DW4CXDEVUS0112.corp.dom>
2018-09-04 15:08:07 -0500

    methods for retrieving all history for a given store added

--- CPUAlert/src/main/java/com/example/demo/data/HistoryRepository.java
+++ CPUAlert/src/main/java/com/example/demo/data/HistoryRepository.java
@@ -17,7 +17,7 @@ import com.example.beans.AlertHistoryId;
 
 public interface HistoryRepository extends JpaRepository<AlertHistory, AlertHistoryId>{
 
-	  @Query("SELECT h FROM AlertHistory h WHERE h.countryCode = :countryCode AND h.ebuNsbr = :ebuNbr")
+	  @Query("SELECT h FROM AlertHistory h WHERE h.alertHistoryId.countryCode = :countryCode AND h.alertHistoryId.ebuNbr = :ebuNbr")
 		public List<AlertHistory> getHistory(@Param ("countryCode")String countryCode, @Param("ebuNbr") int ebuNbr);
 
 }
