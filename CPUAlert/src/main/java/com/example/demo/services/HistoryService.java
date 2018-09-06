d0ce8e32a030aabaefc14f2e04eb805fee0ce5e8
VegetableEater <33704532+VegetableEater@users.noreply.github.com>
2018-09-06 12:38:23 -0500

    more refactoring

--- CPUAlert/src/main/java/com/example/demo/services/HistoryService.java
+++ CPUAlert/src/main/java/com/example/demo/services/HistoryService.java
@@ -1,6 +1,7 @@
 package com.example.demo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
@@ -67,4 +68,14 @@ public interface HistoryService {
 	 * @return AlertHistoryId - the composite ID of an AlertHistory
 	 */
 	public AlertHistoryId getAlertHistoryId(String countryCode, int ebuNbr, Alert alert);
+
+	/**
+	 * 
+	 * @param alert - the alert from which the latest timestamp is pulled
+	 * @param alertHistoryId - the composite ID of alertHistory
+	 * @param timeZone - a String representing some time zone
+	 * @param countryCode - a String representing a country
+	 * @param ebuNbr - the store/club number of an EBU in a specific country
+	 */
+	public void configureAndInsertAlertHistory(Alert alert, AlertHistoryId alertHistoryId, Optional<String> timeZone, String countryCode, int ebuNbr);
 }
