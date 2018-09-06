463b6d8d211a878911808bd6151e994d2a2df0f8
VegetableEater <33704532+VegetableEater@users.noreply.github.com>
2018-09-06 10:22:27 -0500

    fixed merge conflicts, validated inputs, moved relevant code to DAO layer

--- CPUAlert/src/main/java/com/example/beans/AlertHistoryId.java
+++ CPUAlert/src/main/java/com/example/beans/AlertHistoryId.java
@@ -1,130 +1,3 @@
-<<<<<<< HEAD
-package com.example.beans;
-
-import java.io.Serializable;
-import java.time.LocalDateTime;
-
-import javax.persistence.Column;
-import javax.persistence.Embeddable;
-
-import io.swagger.annotations.ApiModel;
-import io.swagger.annotations.ApiModelProperty;
-
-/**
- * Composite Id for AlertHistory
- * 
- * @author Larry Kang
- *
- */
-@Embeddable
-@ApiModel(value = "AlertHistoryId", description = "A composite Id for AlertHistory")
-public class AlertHistoryId implements Serializable{
-
-	@Column(name = "country_code")
-	@ApiModelProperty(value ="CountryCode of the store eg. \"US\" ")
-	private String countryCode;
-	
-	@Column(name = "ebu_nbr")
-	@ApiModelProperty(value ="Store/club number")
-	private int ebuNbr;
-	
-	@Column(name = "alert_type_cd")
-	@ApiModelProperty(value ="Type of alert (eg 15 (ExpressOrder))")
-	private int alertType;
-	
-	@Column(name = "alert_start_ts_gmt")
-	@ApiModelProperty(value ="Timestamp in GMT time when the alert was received")
-	private LocalDateTime alertStartGmt;
-
-	public String getCountryCode() {
-		return countryCode;
-	}
-
-	public void setCountryCode(String countryCode) {
-		this.countryCode = countryCode;
-	}
-
-	public int getEbuNbr() {
-		return ebuNbr;
-	}
-
-	public void setEbuNbr(int ebuNbr) {
-		this.ebuNbr = ebuNbr;
-	}
-
-	public int getAlertType() {
-		return alertType;
-	}
-
-	public void setAlertType(int alertType) {
-		this.alertType = alertType;
-	}
-
-	public LocalDateTime getAlertStartGmt() {
-		return alertStartGmt;
-	}
-
-	public void setAlertStartGmt(LocalDateTime alertStartGmt) {
-		this.alertStartGmt = alertStartGmt;
-	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((alertStartGmt == null) ? 0 : alertStartGmt.hashCode());
-		result = prime * result + alertType;
-		result = prime * result + ((countryCode == null) ? 0 : countryCode.hashCode());
-		result = prime * result + ebuNbr;
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		AlertHistoryId other = (AlertHistoryId) obj;
-		if (alertStartGmt == null) {
-			if (other.alertStartGmt != null)
-				return false;
-		} else if (!alertStartGmt.equals(other.alertStartGmt))
-			return false;
-		if (alertType != other.alertType)
-			return false;
-		if (countryCode == null) {
-			if (other.countryCode != null)
-				return false;
-		} else if (!countryCode.equals(other.countryCode))
-			return false;
-		if (ebuNbr != other.ebuNbr)
-			return false;
-		return true;
-	}
-
-	@Override
-	public String toString() {
-		return "AlertHistoryId [countryCode=" + countryCode + ", ebuNbr=" + ebuNbr + ", alertType=" + alertType
-				+ ", alertStartGmt=" + alertStartGmt + "]";
-	}
-
-	public AlertHistoryId(String countryCode, int ebuNbr, int alertType, LocalDateTime alertStartGmt) {
-		super();
-		this.countryCode = countryCode;
-		this.ebuNbr = ebuNbr;
-		this.alertType = alertType;
-		this.alertStartGmt = alertStartGmt;
-	}
-
-	public AlertHistoryId() {
-		super();
-	}
-
-}
-=======
 package com.example.beans;
 
 import java.io.Serializable;
@@ -168,7 +41,6 @@ public class AlertHistoryId implements Serializable{
 
 	public AlertHistoryId() {
 		super();
-		// TODO Auto-generated constructor stub
 	}
 
 	public AlertHistoryId(String countryCode, int ebuNbr, AlertType alertType, LocalDateTime alertStartGmt) {
@@ -256,8 +128,4 @@ public class AlertHistoryId implements Serializable{
 		return "AlertHistoryId [countryCode=" + countryCode + ", ebuNbr=" + ebuNbr + ", alertType=" + alertType
 				+ ", alertStartGmt=" + alertStartGmt + "]";
 	}
-
-	
-
 }
->>>>>>> 41c1c30de684e5a7673b5ab9fd38dedcdcd222f8
